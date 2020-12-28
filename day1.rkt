e2de3e38a7e76a99d172b9996ade1f48b7767612
ebresafegaga <ebresafegaga@outlook.com>
2020-12-28 18:23:26 +0100

    first star

--- day1.rkt
+++ day1.rkt
@@ -1,1 +1,36 @@
 #lang racket 
+
+(define input-file
+  "/home/ebresafegaga/repos/adventofcode/day1.txt")
+
+(define test-values
+  '(1721
+    979
+    366
+    299
+    675
+    1456))
+
+(define (answer/list numbers)
+  (call/cc
+   (λ (k)
+     (for ([n numbers])
+       (for ([x (remove n numbers)])
+         (if (= 2020 (+ n x))
+             (k (* n x))
+             (void)))))))
+
+(define (lines file)
+  (call-with-input-file file
+    (λ (port)
+      (for/list ([line (in-lines port)])
+        (string->number line)))))
+
+(define (answer/file file)
+  (answer/list (lines file)))
+
+(define (main)
+  (answer/file input-file))
+
+
+
